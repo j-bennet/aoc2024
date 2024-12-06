db1aca873274ff1b39e15bed3134e9d74d2f5b9c
Irina Truong <i.chernyavska@gmail.com>
2024-12-05 23:10:05 -0800

    d6, p2, slow but correct.

--- d06/solution.py
+++ d06/solution.py
@@ -1,3 +1,4 @@
+import copy
 from os import path
 
 ROOT_DIR = path.dirname(__file__)
@@ -44,20 +45,53 @@ class Grid:
             self.g.append(cells)
 
     def is_obstruction(self, x, y):
-        return 0 <= x < self.w and 0 <= y < self.h and self.g[y][x] == "#"
+        return 0 <= x < self.w and 0 <= y < self.h and self.g[y][x] in ("#", "0")
 
     def walk(self, x, y):
         visited = set()
+        direction = self.direction
         while 0 <= x < self.w and 0 <= y < self.h:
             visited.add((x, y))
-            dx, dy = self.moves[self.direction]
+            dx, dy = self.moves[direction]
             if self.is_obstruction(x + dx, y + dy):
-                self.direction = self.turn_right[self.direction]
+                direction = self.turn_right[direction]
                 continue
             else:
                 x += dx
                 y += dy
         return visited
+
+    def in_a_loop(self, corners):
+        if len(corners) < 5:
+            return False
+        # print(corners[-1], corners[-5], corners[-1] == corners[-5])
+        # print(corners)
+        for j in range(len(corners) - 5, -1, -4):
+            if corners[-1] == corners[j]:
+                return True
+        return False
+
+    def will_loop(self, x, y):
+        corners = []
+        direction = self.direction
+        while 0 <= x < self.w and 0 <= y < self.h:
+            dx, dy = self.moves[direction]
+            if self.in_a_loop(corners):
+                return True
+            if self.is_obstruction(x + dx, y + dy):
+                corners.append((x, y))
+                # corners = corners[-100:]
+                direction = self.turn_right[direction]
+                continue
+            else:
+                x += dx
+                y += dy
+        return False
+
+    def __str__(self):
+        data = copy.deepcopy(self.g)
+        data[self.initial[1]][self.initial[0]] = self.direction
+        return "\n".join("".join(row) for row in data)
 
 
 def part1(data):
@@ -69,10 +103,29 @@ def part1(data):
 
 def part2(data):
     """Part 2"""
-    result = 0
-    return result
+    grid = Grid(data)
+    loops = set()
+    for r in range(grid.h):
+        for c in range(grid.w):
+            # if r != 8:
+            #     continue
+            # try adding an obstacle
+            if grid.g[r][c] == ".":
+                grid.g[r][c] = "0"
+                if grid.will_loop(*grid.initial):
+                    # print(f"Loop at {r=}, {c=}")
+                    # print(grid)
+                    # print("----")
+                    loops.add((c, r))
+                # else:
+                #     print(f"No loop at {r=}, {c=}")
+                #     print(grid)
+                #     print("----")
+                # revert the change
+                grid.g[r][c] = "."
+    return len(loops)
 
 
 if __name__ == "__main__":
     print(f"Part 1: {part1(get_data('input.txt'))}")
-    print(f"Part 2: {part2(get_data('example.txt'))}")
+    print(f"Part 2: {part2(get_data('input.txt'))}")

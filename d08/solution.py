c53e2e6cac7f4668ce017face375ea2b87c3485d
Irina Truong <i.chernyavska@gmail.com>
2024-12-08 14:48:52 -0800

    d8 p2

--- d08/solution.py
+++ d08/solution.py
@@ -10,14 +10,18 @@ def get_data(filename="input.txt"):
 
 
 class Graph:
-    def __init__(self, data):
+    def __init__(self, data, part=1):
         self.data = data
         self.h = len(data)
         self.w = len(data[0])
         self.antennas = {}
+        self.antennas_by_point = {}
         self.antenodes = set()
         self.parse()
-        self.find_antenodes()
+        if part == 1:
+            self.find_antenodes()
+        else:
+            self.find_antenodes2()
 
     def parse(self):
         for i, row in enumerate(self.data):
@@ -25,25 +29,62 @@ class Graph:
                 if x != ".":
                     if x not in self.antennas:
                         self.antennas[x] = []
+                    self.antennas_by_point[(i, j)] = x
                     self.antennas[x].append((i, j))
 
-    def find_antenode(self, p1, p2):
+    def find_antenode_for_pair(self, p1, p2):
         i1, j1 = p1
         i2, j2 = p2
-        di = i2 - i1
-        dj = j2 - j1
-        i = i2 + di
-        j = j2 + dj
+        di, dj = i2 - i1, j2 - j1
+        i, j = i2 + di, j2 + dj
         if 0 <= i < self.h and 0 <= j < self.w:
             return (i, j)
 
+    def find_antenodes_for_pair(self, p1, p2):
+        i1, j1 = p1
+        i2, j2 = p2
+        di, dj = i2 - i1, j2 - j1
+        i, j = i2 + di, j2 + dj
+        nodes = set()
+        while 0 <= i < self.h and 0 <= j < self.w:
+            nodes.add((i, j))
+            i, j = i + di, j + dj
+        return nodes
+
+    def find_ante_antenodes_for_pair(self, p1, p2):
+        i1, j1 = p1
+        i2, j2 = p2
+        di, dj = i2 - i1, j2 - j1
+        i, j = i2 + di, j2 + dj
+        nodes = set()
+        while 0 <= i < self.h and 0 <= j < self.w:
+            if (i, j) in self.antennas_by_point:
+                nodes.add((i, j))
+            i, j = i + di, j + dj
+        return nodes
+
     def find_antenodes(self):
-        for atype in self.antennas:
-            for p1 in self.antennas[atype]:
-                for p2 in self.antennas[atype]:
+        for points in self.antennas.values():
+            for p1 in points:
+                for p2 in points:
                     if p1 != p2:
-                        if antenode := self.find_antenode(p1, p2):
+                        if antenode := self.find_antenode_for_pair(p1, p2):
                             self.antenodes.add(antenode)
+
+    def find_antenodes2(self):
+        for points in self.antennas.values():
+            for p1 in points:
+                for p2 in points:
+                    if p1 != p2:
+                        if antenodes := self.find_antenodes_for_pair(p1, p2):
+                            self.antenodes.update(antenodes)
+        antenna_antenodes = set()
+        for p1 in self.antenodes:
+            for p2 in self.antenodes:
+                if p1 != p2:
+                    if antenodes := self.find_ante_antenodes_for_pair(p1, p2):
+                        antenna_antenodes.update(antenodes)
+        self.antenodes.update(antenna_antenodes)
 
     def __str__(self):
         result = []
@@ -68,10 +109,12 @@ def part1(data):
 
 def part2(data):
     """Part 2"""
-    result = 0
-    return result
+    graph = Graph(data, part=2)
+    # print(graph)
+    # print("---")
+    return len(graph.antenodes)
 
 
 if __name__ == "__main__":
     print(f"Part 1: {part1(get_data('input.txt'))}")
-    print(f"Part 2: {part2(get_data('example.txt'))}")
+    print(f"Part 2: {part2(get_data('input.txt'))}")

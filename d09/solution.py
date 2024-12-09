b19eaa805bab6d351d1cec8d72babb24473b7c34
Irina Truong <i.chernyavska@gmail.com>
2024-12-09 00:55:14 -0800

    d9 p2, not working.

--- d09/solution.py
+++ d09/solution.py
@@ -1,3 +1,4 @@
+from dataclasses import dataclass, field
 from os import path
 
 ROOT_DIR = path.dirname(__file__)
@@ -49,6 +50,77 @@ class Disk:
         return "".join(map(str, self.sparse))
 
 
+@dataclass
+class Block:
+    free: int
+    taken: int
+    contents: list[int] = field(default_factory=list)
+
+    def __str__(self):
+        data = self.contents[:]
+        if self.free > 0:
+            data += ["."] * self.free
+        return "".join(map(str, data))
+
+    def can_take(self, other):
+        return self.free >= other.taken
+
+    @property
+    def is_full(self):
+        return self.free == 0
+
+    def take(self, other):
+        self.taken += other.taken
+        self.free -= other.taken
+        self.contents += other.contents
+        other.taken, other.free, other.contents = other.free, other.taken, []
+
+
+class Disk2:
+    def __init__(self, data):
+        self.data = data
+        self.nums = [int(x) for x in data]
+        self.blocks = []
+        self.make_blocks()
+        self.compress()
+
+    def make_blocks(self):
+        file_index = 0
+        for i, n in enumerate(self.nums):
+            if i % 2 == 0:
+                self.blocks.append(Block(0, n, [file_index] * n))
+                file_index += 1
+            else:
+                if n > 0:
+                    self.blocks.append(Block(n, 0, []))
+
+    def maybe_move(self, source_index):
+        for i, block in enumerate(self.blocks):
+            if i >= source_index:
+                break
+            if block.can_take(self.blocks[source_index]):
+                print(
+                    f"Move {self.blocks[source_index]} at {source_index} to {block} at {i}"
+                )
+                block.take(self.blocks[source_index])
+                print(self)
+                return True
+        return False
+
+    def compress(self):
+        for i in range(len(self.blocks) - 1, 0, -1):
+            if self.blocks[i].is_full:
+                self.maybe_move(i)
+
+    @property
+    def checksum(self):
+        nums = str(self).strip(".")
+        return sum(i * int(num) for i, num in enumerate(nums) if num != ".")
+
+    def __str__(self):
+        return "".join(map(str, self.blocks))
+
+
 def part1(data):
     """Part 1"""
     disk = Disk(data[0])
@@ -59,8 +131,11 @@ def part1(data):
 
 def part2(data):
     """Part 2"""
-    result = 0
-    return result
+    disk = Disk2(data[0])
+    # print(disk)
+    # assert str(disk) == "00992111777.44.333....5555.6666.....8888.."
+    # print("-" * 10)
+    return disk.checksum
 
 
 if __name__ == "__main__":

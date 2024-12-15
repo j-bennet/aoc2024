5a594ff4ee37f15e1b4df665810cfdbb8101c84a
Irina Truong <i.chernyavska@gmail.com>
2024-12-15 11:15:56 -0800

    d15 wip.

--- d15/solution.py
+++ d15/solution.py
@@ -4,6 +4,14 @@ from os import path
 ROOT_DIR = path.dirname(__file__)
 
 
+MOVES = {
+    "^": (0, -1),
+    "v": (0, 1),
+    "<": (-1, 0),
+    ">": (1, 0),
+}
+
+
 def get_data(filename="input.txt"):
     full_name = path.join(ROOT_DIR, filename)
     with open(full_name) as f:
@@ -20,9 +28,31 @@ class Robot:
 class Grid:
     w: int
     h: int
-    robot: Robot | None = None
+    robot: Robot
     boxes: set = field(default_factory=set)
     walls: set = field(default_factory=set)
+
+    def move_robot(self, direction: str):
+        dx, dy = MOVES[direction]
+        new_x = self.robot.x + dx
+        new_y = self.robot.y + dy
+        if (new_x, new_y) in self.walls:
+            return False
+
+        def explore_direction(x: int, y: int, boxes_to_move: set):
+            if (x, y) in self.boxes:
+                boxes_to_move.add((x, y))
+                explore_direction(x + dx, y + dy, boxes_to_move)
+            elif 1 < x < (self.w - 1) and 1 < y < (self.h - 1):
+                if (x, y) not in self.walls:
+                    return (x, y)
+
+        boxes_to_move = set()
+        can_move = explore_direction(new_x, new_y, boxes_to_move)
+        if not can_move:
+            return False
+
+        # TODO
 
     def __str__(self):
         result = ""
